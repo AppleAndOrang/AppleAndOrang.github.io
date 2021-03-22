74f23cb1caa28fe6d09922fa67db7c21ca120989
AppleAndOrang <79877946+AppleAndOrang@users.noreply.github.com>
2021-03-22 17:25:27 -0400

    Create gamesetup.js

---
+++ gamesetup.js
@@ -0,0 +1,29 @@
+let canvas = {
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 800;
+    this.canvas.height = 800;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    
+    this.coinInterval = 0;
+    this.game = setInterval(game, 20); // This runs the game function evert 20ms (50fps)
+  },
+  stop : function() {
+    clearInterval(this.game);
+  }
+}
+// Variables go here:
+
+//Some colours if you'd like:
+const playerColour = "rgb(118, 150, 86)";
+const screenColour = "rgb(0, 0, 0)";
+const coinColour = "rgb(221, 175, 12)";
+const textColour = "rgba(255,255,255,0.5)";
+
+// The main game function:
+function game() {
+  canvas.coinInterval++;
+  
+  let ctx = canvas.context;
+}
