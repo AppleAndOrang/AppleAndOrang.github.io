d530e488244b307095e7d78a7d9360c61c05b393
AppleAndOrang <79877946+AppleAndOrang@users.noreply.github.com>
2021-03-22 17:35:12 -0400

    Update and rename game.js to movinggame.js

--- movinggame.js
+++ movinggame.js
@@ -1,41 +1,41 @@
 
-// The function to update your coin position
-function updateCoin() {
-}
-
+let jumpkey = false;
+let rightkey = false;
+let leftkey = false;
 window.addEventListener("keydown", function(event) {   switch(event.code) {
-       case "KeyS":
-       case "ArrowDown":
-           //Movement logic down
-           break;
        case "KeyW":
        case "ArrowUp":
            //Movement logic up
+           jumpkey = true;
            break;
        case "KeyA":
        case "ArrowLeft":
            //Movement logic left
+           leftkey = true;
            break;
        case "KeyD":
        case "ArrowRight":
            //Movement logic right
+           rightkey = true;
            break;
    }
 }, true);
 
 window.addEventListener("keyup", function(event) {
    switch(event.code) {
-       case "KeyS":
-       case "ArrowDown":
        case "KeyW":
        case "ArrowUp":
            //Movement logic vertical stop
+           jumpkey = false;
            break;
        case "KeyA":
        case "ArrowLeft":
+           leftkey = false;
+           break;
        case "KeyD":
        case "ArrowRight":
            //Movement logic horizontal stop
+           rightkey = false;
            break;
    }
 }, true);
