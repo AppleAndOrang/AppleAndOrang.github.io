5c00e2ba4c819bae95532bb805818f4c7a5788d9
AppleAndOrang <79877946+AppleAndOrang@users.noreply.github.com>
2021-05-03 17:39:11 -0400

    Update gamesetup.js

--- gamesetup.js
+++ gamesetup.js
@@ -14,8 +14,21 @@ let canvas = {
 }
 // Variables go here:
 
+let platform = {
+  platform: document.createElement("platform"),
+  start: function () {
+    this.platform.width = 300;
+    this.platform.height = 150;
+    this.context = this.platform.getContext("2d");
+    document.body.insertBefore(this.platform, document.body.childNodes[0])
+  }
+}
+  
+    
+    
 //Some colours if you'd like:
 const playerColour = "rgb(118, 150, 86)";
+const platformColour = "rgb(120, 40, 0)";
 const screenColour = "rgb(0, 0, 0)";
 const coinColour = "rgb(221, 175, 12)";
 const textColour = "rgba(255,255,255,0.5)";
