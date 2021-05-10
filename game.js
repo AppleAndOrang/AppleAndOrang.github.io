4623f4b3cb719987fc735a735ab35ea14c68ad9e
AppleAndOrang <79877946+AppleAndOrang@users.noreply.github.com>
2021-05-10 17:58:09 -0400

    Update game.js

--- game.js
+++ game.js
@@ -47,10 +47,11 @@ function game() {
         }
     }
     
-   if (((y > PlatY + PlatH )< Plat Y) +  ((100 + x) < (PlatY + PlatH))) {
+   if (((y > PlatY && PlatH )< Plat Y) &&  ((100 && x) < (PlatY && PlatH))) {
     y += yvel;
     yvel = 0
    }
+}
 
     if ((x + 100 > coinX && x < coinX + 20) && (y + 100 > coinY && y < coinY + 20)) {
         score++;
